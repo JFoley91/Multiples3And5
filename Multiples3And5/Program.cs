2698afc41ddb2666d411dd3515da6c7ef156e862
JFoley91 <JMFoley91@outlook.com>
2022-02-14 15:45:05 -0700

    Initial Upload with working Code

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -9,7 +9,21 @@ namespace Multiples3And5
     class Program
     {
         static void Main(string[] args)
-        {
+        {       
+            int sum = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                var isMultipleOf3 = i % 3 == 0;
+                var isMultipleOf5 = i % 5 == 0;
+
+                
+                if (isMultipleOf3 || isMultipleOf5)
+                {
+                    
+                    sum += i;
+                }
+            }
+            Console.WriteLine("The sum of all the multiples of 3 and 5 below 1000 is " + sum);
         }
     }
 }
